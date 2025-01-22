28a88b071033814dd6e4e7970819cbc3fa852c81
EricWithC04 <negativegame117@gmail.com>
2025-01-22 11:53:10 -0300

    feat: page of chat

--- chat-tests/src/pages/chat/ChatPage.tsx
+++ chat-tests/src/pages/chat/ChatPage.tsx
@@ -1,8 +1,26 @@
 // import React from 'react'
-// import styles from './ChatPage.module.css'
+import { FaPaperPlane } from 'react-icons/fa'
+import styles from './ChatPage.module.css'
+import exampleImage from '../../assets/profile-example.jpg'
 
 export const ChatPage = () => {
     return (
-        <div>Chat</div>
+        <div className={styles.container}>
+            <div className={styles.navbar}></div>
+            <div className={styles["chat-list"]}></div>
+            <div className={styles["chat-container"]}>
+                <div className={styles["messages-header"]}>
+                    <img src={exampleImage} alt="" />
+                    <h6>Juan Perez</h6>
+                </div>
+                <div className={styles["messages-container"]}></div>
+                <div className={styles["messages-input"]}>
+                    <form>
+                        <input type="text" placeholder='Escribe un mensaje ...'/>
+                        <button type="submit"><FaPaperPlane /></button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }

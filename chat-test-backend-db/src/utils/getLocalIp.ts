4aafd2bfd116610d000ba55dfe729b53e039d3cb
EricWithC04 <negativegame117@gmail.com>
2025-02-02 11:12:13 -0300

    feat: method to obtain local ip

---
+++ chat-test-backend-db/src/utils/getLocalIp.ts
@@ -0,0 +1,11 @@
+export const getLocalIp = () => {
+    const interfaces = require('os').networkInterfaces();
+    for (const name of Object.keys(interfaces)) {
+        for (const iface of interfaces[name]) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return '127.0.0.1';
+}

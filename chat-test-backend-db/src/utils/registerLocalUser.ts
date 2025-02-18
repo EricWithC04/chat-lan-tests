32b7838899bb770b39e3af793e3a00dd080199e0
EricWithC04 <negativegame117@gmail.com>
2025-02-18 20:35:26 -0300

    feat: create chat with a new user detected

--- chat-test-backend-db/src/utils/registerLocalUser.ts
+++ chat-test-backend-db/src/utils/registerLocalUser.ts
@@ -1,4 +1,6 @@
+import { ChatModel } from "../models/chat.model";
 import { ProfileModel } from "../models/profile.model";
+import { ProfileChatTable } from "../models/profile_chat.table";
 
 interface UserData {
     id: string
@@ -16,6 +18,17 @@ export const registerLocalUser = async (userData: UserData) => {
         if (!profileExists) {
             await ProfileModel.create(userData as any)
             console.log("Nuevo usuario registrado");
+
+            const localProfiles: Array<any> = await ProfileModel.findAll({ where: { local: true } });
+
+            localProfiles.forEach(async (profile) => {
+                if (profile.id !== userData.id) {
+                    const newChat: any = await ChatModel.create()
+
+                    await ProfileChatTable.create({ userId: profile.id, chatId: newChat.id }, { logging: false });
+                    await ProfileChatTable.create({ userId: userData.id, chatId: newChat.id }, { logging: false });
+                }
+            });
         }
     } catch (err) {
         console.error(err);

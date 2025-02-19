7471917e036b760ce36f557c99de1815141890ec
EricWithC04 <negativegame117@gmail.com>
2025-02-19 11:03:30 -0300

    feat: logging of userExists hidden

--- chat-test-backend-db/src/utils/localProfileExists.ts
+++ chat-test-backend-db/src/utils/localProfileExists.ts
@@ -2,7 +2,7 @@ import { ProfileModel } from "../models/profile.model"
 
 export const localProfileExists = async (id: string) => {
     try {
-        const profileExists = await ProfileModel.findOne({ where: { id } });
+        const profileExists = await ProfileModel.findOne({ where: { id }, logging: false });
 
         return profileExists ? true : false
     } catch (err) {
